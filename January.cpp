1627b330f1b6badc68d2e92148923718cd1e81e3
Priyesh Prashar <priyesh.prashar@outlook.in>
2024-01-18 20:03:42 +0530

    change code

---
+++ January.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+public:
+
+    int solve(int n, int m) {
+    
+        if (m < n)
+            return 0;
+        if (n == 0)
+            return 1;
+
+        int t = solve(n - 1, m / 2);
+        int nt = solve(n, m - 1);
+
+        return t + nt;
+    }
+
+    int numberSequence(int m, int n) {
+        return solve(n,m);
+        }
+};
